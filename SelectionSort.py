4c51daa898d53c48da821f25130ebbdadb9b1cd0
rkgit-1530 <rajkaranprem@outlook.com>
2026-02-04 14:43:31 +0530

    selection_sort

---
+++ SelectionSort.py
@@ -0,0 +1,10 @@
+l=[20,12,40,25]
+n=len(l)
+print(l)
+for i in range(n-1):
+    min_index=i
+    for j in range(i+1,n):
+        if l[j]<l[min_index]:
+            min_index=j
+    l[i],l[min_index]=l[min_index],l[i]
+print(l)
