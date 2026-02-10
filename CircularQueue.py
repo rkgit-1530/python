95c640fbb28fbb291cf307e79098bbf3914adbc3
rkgit-1530 <rajkaranprem@outlook.com>
2026-02-10 17:07:29 +0530

    Circular Queue Added

---
+++ CircularQueue.py
@@ -0,0 +1,57 @@
+class Queue:
+    def __init__(self,size):
+        self.items = []
+        self.front=-1
+        self.rear=-1
+        self.size=size
+
+    def enqueue(self,data):
+        if self.rear==self.size-1:
+            print("Queue Full")
+        else:
+            if self.front == -1:
+                self.front+=1
+                self.rear=(self.rear+1)%self.size
+            else:
+                self.rear= self.rear+1%self.size
+                self.items.append(data)
+                print(f'{data} has been added')
+    def dequeue(self):
+        if self.front==-1:
+            print("Queue is Empty")
+        else:
+            print(f'{self.items[self.front]} has been deleted')
+            self.front=(self.front+1)%self.size
+            if self.front == self.size:
+                self.front , self.rear=-1,-1
+    def peek(self):
+        if self.front==-1:
+            print("queue is Empty")
+        else:
+            print(f"Peeked Element is {self.items[self.front]}")
+    
+    def isEmpty(self):
+        if self.front==-1:
+            print("Queue is Empty")
+            return True
+    
+    def isFull(self):
+        if self.rear==self.size-1:
+            print("Queue is Full")
+    
+    def display(self):
+        if self.isEmpty:
+            print("Queue is Empty")
+        else:
+            for i in range(self.front,self.rear+1):
+                print(self.items[i],end="->")
+
+size=int(input("Enter Size: "))  
+queue=Queue(size)
+queue.enqueue(5)
+queue.enqueue(10)
+queue.enqueue(15)
+queue.enqueue(20)
+queue.dequeue()
+queue.dequeue()
+queue.display()
